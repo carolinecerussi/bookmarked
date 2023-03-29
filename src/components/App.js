9f5c9d45ee753ae7d47d49881c983bb18a1bd482
Caroline Cerussi <caroceru@gmail.com>
2023-03-29 16:01:30 -0700

    add new personally created background art for books and make custom icons

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import BookControl from "./BookControl";
-import background from "../imgBooks/shiny.jpg";
-
+import background from "../imgBooks/bookTower3.2.png";
+import background1 from "../imgBooks/booktower1.png"
 
 function App() {
 
   return (
 
-    <div style={{ backgroundImage: `url(${background})`, backgroundSize:'cover' }}>
+    <div style={{ backgroundImage: `url(${background1})`, backgroundSize:'cover' }}>
     <React.Fragment>
     <Header />
      <BookControl />    
