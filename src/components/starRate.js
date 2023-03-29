1f6353211ce05768e4c225df6455b83e9759bd9d
Caroline Cerussi <caroceru@gmail.com>
2023-03-29 16:09:37 -0700

    WIP code broken trouble reading value of star--- but working star css and click functionality

--- src/components/starRate.js
+++ src/components/starRate.js
@@ -1,27 +1,12 @@
-import React, { useState } from "react";
+import StarRating from "./StarRating";
+import "./App.css";
 
-const StarRating = () => {
-    const [rating, setRating] = useState(0);
-    const [hover, setHover] = useState(0);
-    return (
-      <div className="star-rating">
-        {[...Array(5)].map((rating, index) => {
-          index += 1;
-          return (
-            <button
-              type="button"
-              key={index}
-              className={index <= (hover || rating) ? "on" : "off"}
-              onClick={() => setRating(index)}
-              
-              onMouseEnter={() => setHover(index)}
-              onMouseLeave={() => setHover(rating)}
-            >
-              <span className="rating">&#9733;</span>
-            </button>
-          );
-        })}
-      </div>
-    );
-  };
-  export default StarRating;
+const App = () => {
+  return (
+    <div className="App">
+      <StarRating />
+    </div>
+  );
+};
+
+export default App;
